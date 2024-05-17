1d674a017e41f6634cac1200442e948e879826f9
Jason Costomiris <jcostom@jasons.org>
2024-05-17 11:48:34 -0400

    first push

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.12.3-slim-bookworm AS builder
+
+ARG TZ=America/New_York
+
+RUN pip install requests python-kasa \
+    && pip cache purge
+
+FROM python:3.12.3-slim-bookworm
+
+ARG TZ=America/New_York
+ARG PYVER=3.12
+
+COPY --from=builder /usr/local/lib/python$PYVER/site-packages/ /usr/local/lib/python$PYVER/site-packages/
+
+RUN mkdir /app
+COPY ./app.py /app
+RUN chmod 755 /app/app.py
+
+ENTRYPOINT [ "python3", "-u", "/app/app.py" ]
---
+++ app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import asyncio
+import logging
+import os
+from time import sleep
+from kasa import SmartPlug
+
+# --- To be passed in to container ---
+# Mandatory vars
+PLUG_IP = os.getenv('PLUG_IP')
+TZ = os.getenv('TZ', 'America/New_York')
+INTERVAL = os.getenv('INTERVAL', 300)
+
+# Optional Vars
+DEBUG = int(os.getenv('DEBUG', 0))
+
+# Other Globals
+VER = "0.1"
+USER_AGENT = f"kasaplugmon/{VER}"
+
+# Setup logger
+LOG_LEVEL = 'DEBUG' if DEBUG else 'INFO'
+logging.basicConfig(level=LOG_LEVEL,
+                    format='[%(levelname)s] %(asctime)s %(message)s',
+                    datefmt='[%d %b %Y %H:%M:%S %Z]')
+logger = logging.getLogger()
+
+
+async def is_plug_on(ip: str) -> bool:
+    p = SmartPlug(ip)
+    await p.update()
+    is_it_on = p.is_on
+    return is_it_on
+
+
+async def plug_on(ip: str) -> None:
+    p = SmartPlug(ip)
+    await p.update()
+    await p.turn_on()
+    DEBUG and logger.debug("Plug turned on.")
+
+
+def main() -> None:
+    logger.info(f"Initiated: {USER_AGENT}")
+    while True:
+        hey_is_this_thing_on = asyncio.run(is_plug_on(PLUG_IP))
+        if hey_is_this_thing_on:
+            DEBUG and logger.debug("Plug state was on.")
+        else:
+            DEBUG and logger.debug("Plug state was off.")
+            asyncio.run(plug_on(PLUG_IP))
+        sleep(INTERVAL)
+
+
+if __name__ == "__main__":
+    main()
